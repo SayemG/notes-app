e2bed8339da68cf0f1c41aa295519392bfb6679c
SayemG <fr9838@wayne.edu>
2023-05-02 21:18:59 -0400

    [notesapp2.s- printing in color]

--- app2.js
+++ app2.js
@@ -1,3 +1,5 @@
+const validator = require('validator')
+const chalk = require("chalk");
 const getNotes = require('./notes.js')
 
 require('./util.js')
@@ -8,3 +10,13 @@ console.log(name)
 
 const notes = getNotes
 console.log(notes())
+
+console.log(validator.isURL('sayem@example.com'))
+
+console.log(chalk.green(validator.isURL('sayem@example.com')))
+
+console.log(chalk.bold.inverse.red(validator.isURL('sayem@example.com')))
+
+
+
+
